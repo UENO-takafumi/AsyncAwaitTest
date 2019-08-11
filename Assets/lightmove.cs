a7936475c9df2d67f9eed3a962123b045dd013aa
asabatabo <asabasan0330@gmail.com>
2019-08-11 17:47:10 +0900

    コミットもれ

---
+++ Assets/lightmove.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class lightmove : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        var angle = this.transform.localEulerAngles;
+        this.transform.localEulerAngles = new Vector3(angle.x, angle.y + 10 * Time.deltaTime, angle.z);
+    }
+}
